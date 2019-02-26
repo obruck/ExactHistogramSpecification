8ef4650ff72b5f9e129aa770eb17fd02c70fd161
obruck <oscar.bruck@helsinki.fi>
2019-02-26 08:55:09 +0200

    Setup-py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(name='ExactHistogramSpecification',
+      version='1.0',
+      url="https://github.com/obruck/ExactHistogramSpecification",
+      author="StefanoD",
+      packages = find_packages()
+      )
